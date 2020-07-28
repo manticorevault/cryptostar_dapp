0cfa17de83ffffc43c0d28059c180a5b9762a4ac
artur <artur_seilah@hotmail.com>
2020-07-28 22:17:33 +0100

    Completed task1: Star Name and Symbol, the lookUptokenIdToStarInfo function, the exchangeStars function and transferStar function

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -14,6 +14,9 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
+
+    string public constant name = "æternum";
+    string public constant symbol = "AET";
     
 
     // mapping the Star with the Owner Address
@@ -56,21 +59,23 @@ contract StarNotary is ERC721 {
 
     // Implement Task 1 lookUptokenIdToStarInfo
     function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
-        //1. You should return the Star saved in tokenIdToStarInfo mapping
+        return tokenIdToStarInfo[_tokenId].name
     }
 
     // Implement Task 1 Exchange Stars function
     function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
-        //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
-        //2. You don't have to check for the price of the token (star)
-        //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
-        //4. Use _transferFrom function to exchange the tokens.
-    }
+        address owner01 = ownerOf(tokenId1);
+        address owner02 = ownerOf(tokenId2);
+
+        require(owner01 == msg.sender || owner2 == msg.sender, "Please, check if the sender is the owner of the star")
+        _transferFrom(owner01, owner02, _tokenId1);
+        _transferFrom(owner02, owner02, _tokenId2);
+    }   
 
     // Implement Task 1 Transfer Stars
     function transferStar(address _to1, uint256 _tokenId) public {
-        //1. Check if the sender is the ownerOf(_tokenId)
-        //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+        require(msg.sender == ownerOf(_tokenId));
+        _transferFrom(msg.sender, _to1, _tokenId);
     }
 
 }
